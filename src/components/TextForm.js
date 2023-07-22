cee5621153f4108937a67fe73e2cedcacd7795a2
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-07-21 21:10:18 -0400

    button update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,15 +4,26 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick= ()=>{
-        console.log("Upper case was clicked"+text);
+    
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleOnChange= (event)=>{
-        console.log("On change");
-        setText(event.target.value);
+    const handleLowClick= ()=>{
+    
+        let newText = text.toLowerCase();
+        setText(newText);
     }
-    const[text, setText] = useState('Enter text here');
+    const handleOnChange= (event)=>{
+        
+             setText(event.target.value);
+        }
+    const handleClrChange= (event)=>{
+        let newText ="";
+        setText(newText);
+           
+       }    
+    
+   const[text, setText] = useState('');
     return (
     <div>
     
@@ -21,7 +32,16 @@ export default function TextForm(props) {
     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
    
   </div>
- <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+ <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+ <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+ <button className="btn btn-primary mx-2" onClick={handleClrChange}>Clear</button>
+ 
+ <h1>Your text summary</h1>
+ <b><p>{text.split(" ").length} words and {text.length} characters</p>
+ <p>{0.008*text.split(" ").length}Minutes to Read</p>
+ </b>
+ <h2>Preview</h2>
+ <p>{text}</p>
   </div>
 
   )
